95d52f3c42ca60671e7e16b1ad3ba6e6ea6923c5
Javier Caballero <caballerojavier13@gmaill.com>
2015-07-10 23:11:38 -0300

    Bug fixing - Grafo de contról de flujo con try

--- src/java/GrafoFlujo/Statements.java
+++ src/java/GrafoFlujo/Statements.java
@@ -51,7 +51,7 @@ import java.util.List;
  * @author javier
  */
 public class Statements {
-
+    
     public static final int BlockStmt = 0;
     public static final int AssertStmt = 1;
     public static final int BreakStmt = 2;
@@ -67,14 +67,14 @@ public class Statements {
     public static final int LabeledStmt = 12;
     public static final int ReturnStmt = 13;
     public static final int SwitchStmt = 14;
-
+    
     public static final int IfStmt = 15;
     public static final int SwitchEntryStmt = 16;
     public static final int ForStmt = 17;
     public static final int ForeachStmt = 18;
     public static final int DoStmt = 19;
     public static final int WhileStmt = 20;
-
+    
     public static int getIndexStatement(Statement s) {
         if (s instanceof AssertStmt) {
             return AssertStmt;
@@ -133,7 +133,7 @@ public class Statements {
                                                                                 if (s instanceof TypeDeclarationStmt) {
                                                                                     return TypeDeclarationStmt;
                                                                                 } else {
-
+                                                                                    
                                                                                     if (s instanceof BlockStmt) {
                                                                                         return BlockStmt;
                                                                                     } else {
@@ -159,11 +159,11 @@ public class Statements {
             }
         }
     }
-
+    
     public static List<Statement> findStatement(MethodDeclaration method, int type) {
         return findStatement(method.getBody(), type);
     }
-
+    
     private static List<Statement> findStatement(Statement s, int type) {
         List<Statement> retorno = new ArrayList();
         if (getIndexStatement(s) == type) {
@@ -258,14 +258,14 @@ public class Statements {
                     break;
             }
         }
-
+        
         return retorno;
     }
-
+    
     public static String nameStatement(Statement s) {
         return nameStatement(getIndexStatement(s));
     }
-
+    
     public static String nameStatement(int type) {
         switch (type) {
             case AssertStmt:
@@ -314,7 +314,7 @@ public class Statements {
                 return "Nada";
         }
     }
-
+    
     public static List<Statement> sentenciasComponentes(Statement s, int type) {
         List<Statement> retorno = new ArrayList();
         List<Statement> stmts;
@@ -354,12 +354,12 @@ public class Statements {
             case WhileStmt:
                 retorno.add(((WhileStmt) s).getBody());
                 break;
-
+            
         }
-
+        
         return retorno;
     }
-
+    
     public static List<Nodo> getNodos(Statement s, Nodo fin, int Id) {
         List<Nodo> resultado = new ArrayList();
         Nodo n1, n2, n3;
@@ -386,11 +386,11 @@ public class Statements {
                         if (index < (stmts.size())) {
                             addNullSiguiente(resultado, ifTemp);
                         }
-
+                        
                         for (Nodo n : TempResult) {
                             resultado.add(n);
                         }
-
+                        
                         index++;
                     }
                 } else {
@@ -413,14 +413,14 @@ public class Statements {
                 resultado.add(n1);
                 ;
                 break;
-
+            
             case DoStmt:
                 try {
                     n1 = new Nodo(Id);
                     n1.setTexto(((DoStmt) s).getCondition().toString());
                     n1.addSiguiente(fin);
                     Id++;
-
+                    
                     nodos = getNodos(((DoStmt) s).getBody(), n1, Id);
                     if (nodos.size() > 0) {
                         n1.addSiguiente(nodos.get(0));
@@ -430,71 +430,71 @@ public class Statements {
                     }
                     resultado.add(n1);
                 } catch (Exception e) {
-
+                    
                 }
                 break;
             case ForStmt:
                 ForStmt forstmt = ((ForStmt) s);
                 Nodo compare = new Nodo(Id);
                 compare.setTexto(forstmt.getCompare().toString());
-
+                
                 compare.addSiguiente(fin);
-
+                
                 Id++;
-
+                
                 List<Nodo> nodosUpdate = getNodosExpresion(forstmt.getUpdate(), compare, Id);
                 Id += nodosUpdate.size();
-
+                
                 List<Nodo> nodosBody = getNodos(forstmt.getBody(), nodosUpdate.get(0), Id);
                 Id += nodosBody.size();
-
+                
                 compare.addSiguiente(nodosBody.get(0));
-
+                
                 List<Nodo> nodosInit = getNodosExpresion(forstmt.getInit(), compare, Id);
                 Id += nodosInit.size();
-
+                
                 for (Nodo nodo : nodosInit) {
                     resultado.add(nodo);
                 }
                 resultado.add(compare);
-
+                
                 for (Nodo nodo : nodosBody) {
                     resultado.add(nodo);
                 }
-
+                
                 for (Nodo nodo : nodosUpdate) {
                     resultado.add(nodo);
                 }
-
+                
                 break;
             case ForeachStmt:
                 ForeachStmt foreachstmt = ((ForeachStmt) s);
-
+                
                 n1 = new Nodo(Id);
-
+                
                 n1.setTexto(foreachstmt.getVariable().getVars().get(0).toString() + ":" + foreachstmt.getIterable().toString() + ".next()");
                 n1.addSiguiente(fin);
                 Id++;
                 nodos = getNodos(foreachstmt.getBody(), n1, Id);
-
+                
                 n1.addSiguiente(nodos.get(0));
-
+                
                 resultado.add(n1);
-
+                
                 for (Nodo nodo : nodos) {
                     resultado.add(nodo);
                 }
-
+                
                 break;
             case IfStmt:
-
+                
                 n1 = new Nodo(Id);
-
+                
                 n1.setTexto(((IfStmt) s).getCondition().toString().trim());
-
+                
                 Id++;
                 resultado.add(n1);
-
+                
                 nodos = getNodos(((IfStmt) s).getThenStmt(), fin, Id);
                 if (nodos.size() > 0) {
                     n1.addSiguiente(nodos.get(0));
@@ -506,13 +506,13 @@ public class Statements {
                     n2 = new Nodo(Id);
                     n2.setTexto("//Then Vacio");
                     n2.addSiguiente(fin);
-
+                    
                     n1.addSiguiente(n2);
                 }
-
+                
                 nodos = getNodos(((IfStmt) s).getElseStmt(), fin, Id);
                 if (nodos.size() > 0) {
-
+                    
                     n1.addSiguiente(nodos.get(0));
                     for (Nodo nodo : nodos) {
                         resultado.add(nodo);
@@ -521,56 +521,61 @@ public class Statements {
                 } else {
                     n1.addSiguiente(fin);
                 }
-
+                
                 break;
             case ReturnStmt:
-
+                
                 n1 = new Nodo(Id);
                 n1.setTexto("RETURN");
                 n1.setRetorno(true);
-
+                
                 n1.addSiguiente(fin);
-
+                
                 resultado.add(n1);
                 break;
             case SwitchStmt:
                 try {
                     nodos = getNodosSwitch(((SwitchStmt) s), fin, Id);
-
+                    
                     for (Nodo nodo : nodos) {
                         resultado.add(nodo);
                     }
                 } catch (Exception e) {
-
+                    
                 }
                 break;
             case ThrowStmt:
                 n1 = new Nodo(Id);
                 n1.setTexto(((ThrowStmt) s).getExpr().toString());
-
+                
                 n1.addSiguiente(fin);
-
+                
                 resultado.add(n1);
                 break;
             case TryStmt:
-
+                Nodo inicio = new Nodo(Id);
+                inicio.setTexto("try");
+                
+                Id++;
+                
                 TryStmt stmtTry = (TryStmt) s;
-
+                
                 finTemp = fin;
-
+                
                 List<Nodo> finallyNodos = getNodos(stmtTry.getFinallyBlock(), fin, Id);
-
+                
                 Id = Id + finallyNodos.size();
                 if (finallyNodos.size() > 0) {
                     finTemp = finallyNodos.get(0);
                 }
+                
                 List<Nodo> tryNodos = new ArrayList();
-
+                
                 stmts = stmtTry.getTryBlock().getStmts();
-
+                
                 for (int j = stmts.size() - 1; j > -1; j--) {
                     nodos = getNodos(stmts.get(j), finTemp, Id);
-
+                    
                     for (Nodo nodo : nodos) {
                         tryNodos.add(nodo);
                         Id++;
@@ -578,58 +583,69 @@ public class Statements {
                     if (nodos.size() > 0) {
                         finTemp = nodos.get(0);
                     }
-
+                    
                 }
-
+                
                 if (finallyNodos.size() > 0) {
                     finTemp = finallyNodos.get(0);
                 } else {
                     finTemp = fin;
                 }
+                
+                inicio.addSiguiente(tryNodos.get(0));
+                
                 List<CatchClause> catchs = stmtTry.getCatchs();
-
+                
                 List<Nodo> catchNodos = new ArrayList();
-
-                Nodo finAlt = null;
-
-                for (int i = 0; i < catchs.size(); i++) {
-                    nodos = getNodosCatch(catchs.get(i), finTemp, null, Id);
-
+                
+                Nodo iniAlt = null;
+                
+                for (CatchClause catch1 : catchs) {
+                    nodos = getNodosCatch(catch1, finTemp, null, Id);
                     Id = Id + nodos.size();
                     if (nodos.size() > 0) {
                         addNullSiguiente(catchNodos, nodos.get(0));
-
+                        
                         for (Nodo nodo : nodos) {
                             catchNodos.add(nodo);
                         }
-
+                        if (iniAlt == null) {
+                            iniAlt = nodos.get(0);
+                        } else {
+                            iniAlt.addSiguiente(nodos.get(0));
+                        }
+                        
                     }
-
                 }
-
+                if (iniAlt != null) {
+                    inicio.addSiguiente(iniAlt);
+                }
+                
                 addNullSiguiente(catchNodos, tryNodos.get(0));
-
-                resultado.addAll(catchNodos);
+                resultado.add(inicio);
+                
                 resultado.addAll(tryNodos);
+                resultado.addAll(catchNodos);
+                
                 resultado.addAll(finallyNodos);
-
+                
                 break;
             case TypeDeclarationStmt:
                 n1 = new Nodo(Id);
                 n1.setTexto(((TypeDeclarationStmt) s).toString());
-
+                
                 n1.addSiguiente(fin);
-
+                
                 resultado.add(n1);
                 break;
             case WhileStmt:
                 try {
                     n1 = new Nodo(Id);
                     n1.setTexto(((WhileStmt) s).getCondition().toString());
-
+                    
                     n1.addSiguiente(fin);
                     Id++;
-
+                    
                     resultado.add(n1);
                     nodos = getNodos(((WhileStmt) s).getBody(), n1, Id);
                     if (nodos.size() > 0) {
@@ -639,33 +655,33 @@ public class Statements {
                         }
                     }
                 } catch (Exception e) {
-
+                    
                 }
                 break;
             case EmptyStmt:
-
+                
                 n1 = new Nodo(Id);
-
+                
                 n1.setTexto("Empty");
-
+                
                 n1.addSiguiente(fin);
                 resultado.add(n1);
-
+                
                 break;
             case ExplicitConstructorInvocationStmt:
                 System.out.println("Llamada a constructor");
                 n1 = new Nodo(Id);
                 n1.setTexto(((ExplicitConstructorInvocationStmt) s).getExpr().toString());
-
+                
                 n1.addSiguiente(fin);
-
+                
                 resultado.add(n1);
                 break;
             case ExpressionStmt:
                 n1 = new Nodo(Id);
-
+                
                 n1.setTexto(((ExpressionStmt) s).getExpression().toString());
-
+                
                 n1.addSiguiente(fin);
                 resultado.add(n1);
                 break;
@@ -680,93 +696,93 @@ public class Statements {
         }
         return resultado;
     }
-
+    
     private static List<Nodo> reverseNodos(List<Nodo> nodos) {
         List<Nodo> resultado = new ArrayList();
-
+        
         for (int i = (nodos.size() - 1); i > -1; i--) {
             resultado.add(nodos.get(i));
         }
-
+        
         return resultado;
     }
-
+    
     private static List<Nodo> getNodosCatch(CatchClause s, Nodo fin, Nodo finAlt, int Id) {
         List<Nodo> resultado = new ArrayList();
-
+        
         Nodo excepcion = new Nodo(Id);
-
+        
         Id++;
-
+        
         List<Nodo> nodos = getNodos(s.getCatchBlock(), fin, Id);
-
+        
         excepcion.addSiguiente(nodos.get(0));
         excepcion.setTexto(s.getExcept().getType().toString());
         excepcion.addSiguiente(finAlt);
         resultado.add(excepcion);
-
+        
         resultado.addAll(nodos);
-
+        
         return resultado;
     }
-
+    
     private static List<Nodo> getNodosSwitch(SwitchStmt s, Nodo fin, int Id) {
         List<Nodo> resultado = new ArrayList();
         List<SwitchEntryStmt> entries = s.getEntries();
-
+        
         Nodo primerNodo = null;
         try {
             primerNodo = new Nodo(Id);
             primerNodo.setTexto(s.getSelector().toString());
             Id++;
             for (int i = entries.size() - 1; i > -1; i--) {
-
+                
                 SwitchEntryStmt switchEntryStmt = entries.get(i);
-
+                
                 Nodo label = new Nodo(Id);
-
+                
                 try {
                     label.setTexto(s.getSelector().toString() + " == " + switchEntryStmt.getLabel().toString());
-
+                    
                 } catch (NullPointerException e) {
                     label.setTexto("Por defecto");
-
+                    
                 }
                 resultado.add(label);
                 primerNodo.addSiguiente(label);
-
+                
                 Id++;
-
+                
                 List<Nodo> blocks = new ArrayList();
-
+                
                 List<Statement> stmts = switchEntryStmt.getStmts();
-
+                
                 Nodo finTemp = fin;
                 for (int j = stmts.size() - 1; j > -1; j--) {
                     List<Nodo> nodos = getNodos(stmts.get(j), finTemp, Id);
-
+                    
                     if (nodos.size() > 0) {
                         finTemp = nodos.get(0);
-
+                        
                         for (Nodo nodo : nodos) {
                             resultado.add(nodo);
                             Id++;
                         }
                     }
-
+                    
                 }
                 label.addSiguiente(finTemp);
-
+                
             }
             resultado.add(primerNodo);
             resultado = reverseNodos(resultado);
-
+            
         } catch (Exception e) {
-
+            
         }
         return resultado;
     }
-
+    
     private static List<Nodo> getNodosExpresion(List<Expression> listado, Nodo fin, int Id) {
         Nodo finTemp = fin;
         List<Nodo> Resultado = new ArrayList();
@@ -778,10 +794,10 @@ public class Statements {
             finTemp = n;
             Id++;
         }
-
+        
         return Resultado;
     }
-
+    
     private static void addNullSiguiente(List<Nodo> nodos, Nodo fin) {
         for (Nodo nodo : nodos) {
             if (nodo.getSiguiente().size() < 1) {
@@ -789,5 +805,5 @@ public class Statements {
             }
         }
     }
-
+    
 }
